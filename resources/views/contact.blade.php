8d3a7e5bd4bd47c057e4a2248e0e5784ab860923
Milos Barjaktarovic <bmilos1983@gmail.com>
2025-12-16 13:15:23 +0100

    Laravel domaci 1

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>contact Us</title>
+</head>
+
+<body>
+    <h1>Contact Our Company</h1>
+    <p>Feel free to reach out to us with any questions or concerns you may have.</p>
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Welcome</title>
+</head>
+
+<body>
+    <h1>Welcome to Shop</h1>
+
+    <nav>
+        <a href="/">Welcome</a>
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/shop">Shop</a>
+        <a href="/contact">Contact</a>
+    </nav>
+
+    <p>Ovo je glavna stranica.</p>
+</body>
+
+</html>
